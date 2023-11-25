92c0e6d8bd0edb91e9957eaedb0048947317cb67
Dennis Boachie Boateng <dennisboachie9@gmail.com>
2023-11-25 16:37:09 +0000

    dijkstra algorithm

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class Graph(object):
         self.weights[(from_node, to_node)] = weight
 
 
-graph = Graph()
+new_graph = Graph()
 
 edges = [
     ('X', 'A', 7),
@@ -43,3 +43,29 @@ edges = [
     ('J', 'L', 1),
     ('K', 'Y', 5),
 ]
+
+for edge in edges:
+    new_graph.add_node(*edge)
+
+print(new_graph.edges)
+
+def dijkstra(graph, initial, end):
+    # shortest paths is a dict of nodes
+    # whose value is a tuple of (previous node, weight)
+    # visited is a set()
+    shortest_paths = {initial: (None, 0)}
+    current_node = initial
+    visited = set()
+
+    # While the current node is not equal to the end node
+    # Add current node the visited nodes set
+    # Get all possible destinations of current node from the shortest_paths dict
+    # Get the weight to the current node to the prev node
+
+    # for next_node in destinations:
+    # compute the next weight given the prev weight
+    # Update shortest_path dict, else
+    #
+
+
+
