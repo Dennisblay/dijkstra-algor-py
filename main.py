ddf92eb962a7ea3d09a2a05b028ebef2cff309a0
Dennis Boachie Boateng <dennisboachie9@gmail.com>
2023-11-25 13:17:53 +0000

    Graph implementation

--- main.py
+++ main.py
@@ -12,3 +12,34 @@ class Graph(object):
                e.g. {('X', 'A'): 7, ('X', 'B'): 2, ...}
                """
         self.edges = defaultdict(list)
+        self.weights = {}
+
+    def add_node(self, from_node, to_node, weight):
+        # Note: assumes edges are bi-directional
+        self.edges[from_node].append(to_node)
+        self.edges[to_node].append(from_node)
+        self.weights[(from_node, to_node)] = weight
+
+
+graph = Graph()
+
+edges = [
+    ('X', 'A', 7),
+    ('X', 'B', 2),
+    ('X', 'C', 3),
+    ('X', 'E', 4),
+    ('A', 'B', 3),
+    ('A', 'D', 4),
+    ('B', 'D', 4),
+    ('B', 'H', 5),
+    ('C', 'L', 2),
+    ('D', 'F', 1),
+    ('F', 'H', 3),
+    ('G', 'H', 2),
+    ('G', 'Y', 2),
+    ('I', 'J', 6),
+    ('I', 'K', 4),
+    ('I', 'L', 4),
+    ('J', 'L', 1),
+    ('K', 'Y', 5),
+]
